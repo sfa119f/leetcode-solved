dc344236c8100e02f4e91afe30a8cae43d69c9f4
Syarifuddin <47862364+sfa119f@users.noreply.github.com>
2024-06-13 22:55:46 +0700

    Time: 52 ms (90.86%), Space: 56.3 MB (5.08%) - LeetHub

--- 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.js
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.js
@@ -3,14 +3,36 @@
  * @param {number[]} students
  * @return {number}
  */
+
 var minMovesToSeat = function(seats, students) {
-  seats.sort((a, b) => a - b)
-  students.sort((a, b) => a - b)
-  
-  let total = 0
-  for(let i=0; i<seats.length; i++) {
-    total += Math.abs(seats[i] - students[i])
-  }
+  seats = mergeSort(seats)
+  students = mergeSort(students)
+
+  const total = seats.reduce((total, val, idx) =>
+    total + Math.abs(val - students[idx])
+  , 0)
   
   return total
 };
+
+function merge(left, right) {
+    let arr = [];
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            arr.push(left.shift());
+        } else {
+            arr.push(right.shift());
+        }
+    }
+    return [...arr, ...left, ...right];
+}
+
+function mergeSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+    const middle = Math.floor(arr.length / 2);
+    const left = arr.slice(0, middle);
+    const right = arr.slice(middle);
+    return merge(mergeSort(left), mergeSort(right));
+}
