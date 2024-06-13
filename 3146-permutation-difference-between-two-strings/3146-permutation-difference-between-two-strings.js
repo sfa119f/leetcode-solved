1389f20489b60658492516e01e2c04522b768d89
Syarifuddin <47862364+sfa119f@users.noreply.github.com>
2024-06-14 01:58:08 +0700

    Time: 46 ms (99.14%), Space: 50.6 MB (93.39%) - LeetHub

--- 3146-permutation-difference-between-two-strings/3146-permutation-difference-between-two-strings.js
+++ 3146-permutation-difference-between-two-strings/3146-permutation-difference-between-two-strings.js
@@ -4,18 +4,9 @@
  * @return {number}
  */
 var findPermutationDifference = function(s, t) {
-  const temp = {}
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] in temp) temp[s[i]] += i
-    else temp[s[i]] = i
-    
-    if (t[i] in temp) temp[t[i]] -= i
-    else temp[t[i]] = i * -1
-  }
-  
   let total = 0
-  for (const key in temp) {
-    total += Math.abs(temp[key])
+  for (let i = 0; i < s.length; i++) {
+    total += Math.abs(i - t.indexOf(s[i]))
   }
   return total
 };
