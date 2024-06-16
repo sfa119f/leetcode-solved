ed9d2ae6a8b8b174eec036474ca6074f5b82c4cd
Syarifuddin <47862364+sfa119f@users.noreply.github.com>
2024-06-16 23:36:05 +0700

    Time: 62 ms (19.23%), Space: 49.6 MB (21.15%) - LeetHub

---
+++ 0330-patching-array/0330-patching-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number}
+ */
+var minPatches = function(nums, n) {
+  let temp = 1
+  let added = 0
+  let idx = 0
+  while (temp <= n) {
+    if (idx < nums.length && nums[idx] <= temp) {
+      temp += nums[idx]
+      idx++
+    } else {
+      temp += temp
+      added++
+    }
+  }
+  return added
+};
