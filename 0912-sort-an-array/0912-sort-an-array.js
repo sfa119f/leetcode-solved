57760c3a3ac1225a78eaa1c4004a1512f3b88b18
Syarifuddin <47862364+sfa119f@users.noreply.github.com>
2024-07-25 15:47:28 +0700

    Time: 168 ms (96.83%), Space: 78 MB (5.19%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.js
+++ 0912-sort-an-array/0912-sort-an-array.js
@@ -3,27 +3,19 @@
  * @return {number[]}
  */
 var sortArray = function(nums) {
-  return mergeSort(nums)
-};
-
-function merge(left, right) {
-  let arr = [];
-  while (left.length && right.length) {
-    if (left[0] < right[0]) {
-      arr.push(left.shift());
-    } else {
-      arr.push(right.shift());
+  const minValue = Math.min(...nums)
+  const maxValue = Math.max(...nums)
+  
+  const arrCount = Array(maxValue - minValue + 1).fill(0)
+  for (const num of nums) {
+    arrCount[num - minValue]++
+  }
+  
+  const result = []
+  for (let i = 0; i < maxValue - minValue + 1; i++) {
+    for (let j = 0; j < arrCount[i]; j++) {
+      result.push(i + minValue)
     }
   }
-  return [...arr, ...left, ...right];
-}
-
-function mergeSort(arr) {
-  if (arr.length <= 1) {
-    return arr;
-  }
-  const middle = Math.floor(arr.length / 2);
-  const left = arr.slice(0, middle);
-  const right = arr.slice(middle);
-  return merge(mergeSort(left), mergeSort(right));
-}
+  return result
+};
