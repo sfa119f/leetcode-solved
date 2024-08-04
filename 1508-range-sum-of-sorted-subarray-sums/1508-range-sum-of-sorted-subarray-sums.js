ad3e3d2a1dd5e353d9924f1a72c3700de3bc353c
Syarifuddin <47862364+sfa119f@users.noreply.github.com>
2024-08-04 22:11:52 +0700

    Time: 311 ms (42.31%), Space: 81.5 MB (57.69%) - LeetHub

---
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+var rangeSum = function(nums, n, left, right) {
+  const sumNums = []
+  
+  for (let i = 0; i < n; i++) {
+    let temp = 0
+    for (let j = i; j < n; j++) {
+      temp += nums[j]
+      sumNums.push(temp)
+    }
+  }
+  
+  sumNums.sort((a, b) => a - b)
+  
+  let res = 0
+  for (let i = left - 1; i < right; i++) {
+    res = (res + sumNums[i]) % (Math.pow(10, 9) + 7)
+  }
+  return res
+};
