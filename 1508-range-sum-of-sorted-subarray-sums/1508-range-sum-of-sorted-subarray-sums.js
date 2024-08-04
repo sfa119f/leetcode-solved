04974966c1f31c0f84ec9d0a2b8f70f1b7e8804f
Syarifuddin <47862364+sfa119f@users.noreply.github.com>
2024-08-04 22:14:56 +0700

    Time: 70 ms (100.00%), Space: 55.4 MB (100.00%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.js
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.js
@@ -6,21 +6,49 @@
  * @return {number}
  */
 var rangeSum = function(nums, n, left, right) {
-  const sumNums = []
-  
-  for (let i = 0; i < n; i++) {
-    let temp = 0
-    for (let j = i; j < n; j++) {
-      temp += nums[j]
-      sumNums.push(temp)
+  const MODULUS = 1000000007
+
+  function countAndSumSubarrays(array, threshold) {
+    let count = 0
+    let totalSum = 0n
+    let currentWindowSum = 0n
+    let runningSum = 0n
+    let start = 0
+
+    for (let end = 0; end < array.length; end++) {
+      runningSum += BigInt(array[end]) * BigInt(end - start + 1)
+      currentWindowSum += BigInt(array[end])
+      while (currentWindowSum > threshold) {
+        runningSum -= currentWindowSum
+        currentWindowSum -= BigInt(array[start])
+        start++
+      }
+      count += end - start + 1
+      totalSum += runningSum
     }
+    return [count, totalSum]
   }
-  
-  sumNums.sort((a, b) => a - b)
-  
-  let res = 0
-  for (let i = left - 1; i < right; i++) {
-    res = (res + sumNums[i]) % 1000000007
+
+  function calculateSumOfFirstKSubarrays(array, k) {
+    let low = BigInt(Math.min(...array))
+    let high = BigInt(array.reduce((a, b) => a + b, 0))
+
+    while (low < high) {
+      let mid = low + (high - low) / 2n
+      if (countAndSumSubarrays(array, mid)[0] < k) {
+        low = mid + 1n
+      } else {
+        high = mid
+      }
+    }
+    const [count, sum] = countAndSumSubarrays(array, low)
+    return sum - low * BigInt(count - k)
   }
-  return res
+
+  const result = (
+    calculateSumOfFirstKSubarrays(nums, right) -
+    calculateSumOfFirstKSubarrays(nums, left - 1) +
+    BigInt(MODULUS)
+  ) % BigInt(MODULUS)
+  return Number(result)
 };
