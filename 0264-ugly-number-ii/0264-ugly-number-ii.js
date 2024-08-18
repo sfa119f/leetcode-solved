b9f8641eeb3193e0a1483f8ca2ec6ba276d03407
Syarifuddin <47862364+sfa119f@users.noreply.github.com>
2024-08-18 19:09:45 +0700

    Time: 59 ms (72.27%), Space: 53.3 MB (25.21%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.js
+++ 0264-ugly-number-ii/0264-ugly-number-ii.js
@@ -15,16 +15,9 @@ var nthUglyNumber = function(n) {
     let next = Math.min(Math.min(c2, c3), c5)
     out.push(next)
 
-    if (next === c2) {
-      i2++
-    }
-    if (next === c3) {
-      i3++
-    }
-    if (next === c5) {
-      i5++
-    } 
-
+    if (next === c2) i2++
+    if (next === c3) i3++
+    if (next === c5) i5++
   }
 
   return out[n-1]
