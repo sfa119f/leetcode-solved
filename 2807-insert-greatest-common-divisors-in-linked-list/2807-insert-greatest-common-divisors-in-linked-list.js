fd362013e4ce2ca07277731b3e9812a6f95509f6
Syarifuddin <47862364+sfa119f@users.noreply.github.com>
2024-09-10 12:39:08 +0700

    Time: 101 ms (49.74%), Space: 58.4 MB (50.26%) - LeetHub

--- 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.js
+++ 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.js
@@ -11,8 +11,10 @@
  */
 var insertGreatestCommonDivisors = function(head) {
   const GCD = (a, b) => {
-    if (b === 0) return a
-    return GCD(b, a % b)
+    while (b !== 0) {
+      [a, b] = [b, a % b];
+    }
+    return a;
   }
   
   let node = head
