04a1074ec6134ae112410e472340cde4044e4259
Syarifuddin <47862364+sfa119f@users.noreply.github.com>
2024-09-21 17:16:27 +0700

    Time: 74 ms (80.36%), Space: 56.2 MB (83.93%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.js
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.js
@@ -3,10 +3,16 @@
  * @return {number[]}
  */
 var lexicalOrder = function(n) {
-  const arr = []
-  for (let i = 1; i <= n; i++) {
-    arr.push(i.toString())
+  const result = []
+
+  const pushFromTo = (start, end) => {
+    while(start<=end && start<=n) { 
+      result.push(start)
+      pushFromTo(start*10, start*10+9)
+      start++   
+    }
   }
-  arr.sort()
-  return arr.map(v => Number(v))
+
+  pushFromTo(1, 9)
+  return result
 };
