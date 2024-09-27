9c1b8c783cd925c4470de518a31b7c4a49a9a934
Syarifuddin <47862364+sfa119f@users.noreply.github.com>
2024-09-27 16:49:13 +0700

    Time: 141 ms (72.34%), Space: 59.7 MB (38.30%) - LeetHub

---
+++ 0731-my-calendar-ii/0731-my-calendar-ii.js
@@ -0,0 +1,35 @@
+
+var MyCalendarTwo = function() {
+  this.calendar = []
+  this.overlaps = []
+};
+
+/** 
+ * @param {number} start 
+ * @param {number} end
+ * @return {boolean}
+ */
+MyCalendarTwo.prototype.book = function(start, end) {
+  for (let date of this.overlaps) {
+    if (start < date[1] && end > date[0]) {
+      return false
+    }
+  }
+
+  for (let date of this.calendar) {
+    if (start < date[1] && end > date[0]) {
+      this.overlaps.push([
+        Math.max(date[0], start), 
+        Math.min(date[1], end)
+      ])
+    }
+  }
+  this.calendar.push([start, end])
+  return true
+};
+
+/** 
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * var obj = new MyCalendarTwo()
+ * var param_1 = obj.book(start,end)
+ */
