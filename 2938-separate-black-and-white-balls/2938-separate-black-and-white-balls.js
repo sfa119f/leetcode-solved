d246102c9d5150cec2fe7f84dc019e7b6a258e3d
Syarifuddin <47862364+sfa119f@users.noreply.github.com>
2024-10-15 23:48:39 +0700

    Time: 78 ms (43.64%), Space: 53.1 MB (65.45%) - LeetHub

--- 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.js
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.js
@@ -3,29 +3,11 @@
  * @return {number}
  */
 var minimumSteps = function(s) {
-  const n = s.length
-  const blackIdx = []
-  let blackCount = 0
+  let count = 0, steps = 0
   
-  for (let i = n - 1; i >= 0; i--) {
-    if (s[i] == '1') {
-      blackIdx.push(i)
-      blackCount += 1
-    }
-  }
-  
-  if (blackCount == 0) return 0
-  if (blackCount == 1) return n - 1 - blackIdx[0]
-  if (
-    blackIdx[0] - blackIdx[blackCount - 1] == blackCount - 1 && 
-    blackIdx[0] == n - 1
-  ) {
-    return 0
-  }
-  
-  let steps = 0
-  for (let i = 0; i < blackCount; i++) {
-    steps += (n - 1 - i - blackIdx[i])
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] == 0) count++
+    else steps += count
   }
   
   return steps
