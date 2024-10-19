53acdefd2cf8e3ec8c9ea5b687c594a27a2a515c
Syarifuddin <47862364+sfa119f@users.noreply.github.com>
2024-10-19 22:36:00 +0700

    Time: 0 ms (100.00%), Space: 48.9 MB (61.82%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.js
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.js
@@ -4,16 +4,24 @@
  * @return {character}
  */
 var findKthBit = function(n, k) {
-  let s = "0"
-  let i = 1
-  while (Math.pow(2, i) - 1 < k && n != i) {
-    i++
-    let temp = s
-    s += "1"
-    for (let j = Math.pow(2, i - 1) - 2; j >= 0; j--) {
-      if (temp[j] == "0") s += "1"
-      else s += "0"
-    }
+  let s='0'
+  let noda = [1]
+  for (let i = 1; i < n; i++){
+    noda[i] = 2 * noda[i-1] + 1
   }
-  return s[k - 1]
+  function findDigit(i,n){
+    if (n == 0) return '0'
+    
+    let mid = (noda[n] - 1) / 2
+    if (i == mid) {
+      return '1'
+    } else if (i < mid) {
+      return findDigit(i,n-1)
+    } else if (findDigit(mid - (i - mid), n - 1) == '0') {
+      return '1'
+    }
+    return '0'
+  }
+  
+  return findDigit(k-1,n-1)
 };
