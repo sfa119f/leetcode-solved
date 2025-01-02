5e2ab17a61cc03bed708df2efeee03784463d806
Syarifuddin <47862364+sfa119f@users.noreply.github.com>
2025-01-02 10:49:26 +0700

    Time: 15 ms, Memory: 75.9 MB - LeetHub

---
+++ 0546-33-2559-count-vowel-strings-in-ranges/0546-33-2559-count-vowel-strings-in-ranges.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string[]} words
+ * @param {number[][]} queries
+ * @return {number[]}
+ */
+var vowelStrings = function(words, queries) {
+  const n = words.length
+  const vowelLetter = ['a', 'e', 'i', 'o', 'u']
+
+  const countVowel = new Array(n + 1).fill(0)
+  for (let i = 0; i < n; i++) {
+    if (
+      vowelLetter.includes(words[i].at(0)) && 
+      vowelLetter.includes(words[i].at(-1))
+    ) {
+      countVowel[i + 1] = countVowel[i] + 1
+    } else {
+      countVowel[i + 1] = countVowel[i]
+    }
+  }
+
+  const result = []
+  for (let [l, r] of queries) {
+    result.push(countVowel[r + 1] - countVowel[l])
+  }
+  return result
+};
