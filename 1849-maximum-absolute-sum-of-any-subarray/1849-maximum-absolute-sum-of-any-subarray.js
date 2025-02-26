f3aa323e5b6e28f40e57082464fb215c4a72eb72
Syarifuddin <47862364+sfa119f@users.noreply.github.com>
2025-02-27 06:53:27 +0700

    Time: 2 ms (86.96%), Space: 60.4 MB (28.26%) - LeetHub

---
+++ 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxAbsoluteSum = function(nums) {
+  let sum = 0, minSum = 0, maxSum = 0
+  for (let num of nums) {
+    sum += num
+    if (sum > maxSum) maxSum = sum
+    if (sum < minSum) minSum = sum
+  }
+  return Math.abs(maxSum - minSum)
+};
