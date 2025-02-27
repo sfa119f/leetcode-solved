599ee160f7eee01250130d774446fa2f19770a6f
Syarifuddin <47862364+sfa119f@users.noreply.github.com>
2025-02-27 09:23:42 +0700

    Time: 8 ms (40.75%), Space: 67 MB (26.63%) - LeetHub

---
+++ 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var numOfSubarrays = function(arr) {
+  let oddCount = 0, prefixSum = 0
+  for (let a of arr) {
+      prefixSum += a
+      oddCount += prefixSum % 2
+  }
+  oddCount += (arr.length - oddCount) * oddCount
+  return oddCount % 1_000_000_007
+};
