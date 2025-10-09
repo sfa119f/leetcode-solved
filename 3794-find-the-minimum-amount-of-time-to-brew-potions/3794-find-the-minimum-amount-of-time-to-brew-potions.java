3de0a63aed8a3840392cc8ea3524e2fb9780a01f
Syarifuddin <47862364+sfa119f@users.noreply.github.com>
2025-10-09 23:31:49 +0700

    Time: 139 ms (49.29%), Space: 45 MB (28.17%) - LeetHub

--- 3794-find-the-minimum-amount-of-time-to-brew-potions/3794-find-the-minimum-amount-of-time-to-brew-potions.java
+++ 3794-find-the-minimum-amount-of-time-to-brew-potions/3794-find-the-minimum-amount-of-time-to-brew-potions.java
@@ -1,36 +1,17 @@
 class Solution {
     public long minTime(int[] skill, int[] mana) {
-        int n = skill.length;
-        int m = mana.length;
-
-        int[] preSumSkill = new int[n];
-        preSumSkill[0] = skill[0];
-        for (int i = 1; i < n; i++) {
-            preSumSkill[i] = preSumSkill[i - 1] + skill[i];
-        }
-
-        long[] tempDone = countTime(preSumSkill, mana[0]);
-        for (int i = 1; i < m; i++) {
-            long[] needTime = countTime(preSumSkill, mana[i]);
-            int baseOnIdx = 0;
-            for (int j = 1; j < n; j++) {
-                if (tempDone[baseOnIdx] + needTime[j - 1] - (baseOnIdx == 0 ? 0 : needTime[baseOnIdx - 1]) < tempDone[j]) {
-                    baseOnIdx = j;
-                }
+        int n = skill.length, m = mana.length;
+        long[] done = new long[n + 1];
+        
+        for (int j = 0; j < m; ++j) {
+            for (int i = 0; i < n; ++i) {
+                done[i + 1] = Math.max(done[i + 1], done[i]) + (long) mana[j] * skill[i];
             }
-            long startTime = tempDone[baseOnIdx] - (baseOnIdx == 0 ? 0 : needTime[baseOnIdx - 1]);
-            for (int j = 0; j < n; j++) {
-                tempDone[j] = startTime + needTime[j];
+            for (int i = n - 1; i > 0; --i) {
+                done[i] = done[i + 1] - (long) mana[j] * skill[i];
             }
         }
-        return tempDone[n - 1];
-    }
-
-    private long[] countTime(int[] preSum, int mana) {
-        long[] res = new long[preSum.length];
-        for (int i = 0; i < preSum.length; i++) {
-            res[i] = (long) preSum[i] * mana;
-        }
-        return res;
+        
+        return done[n];
     }
 }
