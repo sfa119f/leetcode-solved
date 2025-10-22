4cfdb560358df357afb45c6c6792c31fec0d25eb
Syarifuddin <47862364+sfa119f@users.noreply.github.com>
2025-10-22 23:01:04 +0700

    Time: 464 ms (48.61%), Space: 73.3 MB (48.61%) - LeetHub

--- 3640-maximum-frequency-of-an-element-after-performing-operations-ii/3640-maximum-frequency-of-an-element-after-performing-operations-ii.java
+++ 3640-maximum-frequency-of-an-element-after-performing-operations-ii/3640-maximum-frequency-of-an-element-after-performing-operations-ii.java
@@ -1,55 +1,60 @@
 class Solution {
+    class Helper {
+        private int[] nums;
+        private int k;
+        private int numOperations;
+        private int n;
+        private Map<Integer, Integer> numFreq = new HashMap<>();
+
+        public Helper(int[] nums, int k, int numOperations) {
+            this.nums = nums;
+            this.k = k;
+            this.numOperations = numOperations;
+            this.n = nums.length;
+            for (int num : nums) {
+                numFreq.put(num, numFreq.getOrDefault(num, 0) + 1);
+            }
+        }
+
+        public int check(int num) {
+            int freq = numFreq.getOrDefault(num, 0);
+            int lower = lowerBound(num - k);
+            int higher = upperBound(num + k);
+            return Math.min(numOperations, higher - lower - freq) + freq;
+        }
+
+        private int lowerBound(int target) {
+            int left = 0, right = n;
+            while (left < right) {
+                int mid = (left + right) / 2;
+                if (nums[mid] < target) left = mid + 1;
+                else right = mid;
+            }
+            return left;
+        }
+        
+        private int upperBound(int target) {
+            int left = 0, right = n;
+            while (left < right) {
+                int mid = (left + right) / 2;
+                if (nums[mid] <= target) left = mid + 1;
+                else right = mid;
+            }
+            return left;
+        }
+    }
+
     public int maxFrequency(int[] nums, int k, int numOperations) {
         Arrays.sort(nums);
         int n = nums.length;
 
-        class Helper {
-            private int[] nums;
-            private int k;
-            private int numOperations;
-
-            public Helper(int[] nums, int k, int numOperations) {
-                this.nums = nums;
-                this.k = k;
-                this.numOperations = numOperations;
-            }
-    
-            public int check(int num) {
-                int l = lowerBound(num);
-                int h = upperBound(num);
-                int ll = lowerBound(num - k);
-                int hh = upperBound(num + k);
-                int res = (hh - h) + (l - ll);
-                return Math.min(numOperations, res) + (h - l);
-            }
-
-            private int lowerBound(int target) {
-                int left = 0, right = n;
-                while (left < right) {
-                    int mid = (left + right) / 2;
-                    if (nums[mid] < target) left = mid + 1;
-                    else right = mid;
-                }
-                return left;
-            }
-            
-            private int upperBound(int target) {
-                int left = 0, right = n;
-                while (left < right) {
-                    int mid = (left + right) / 2;
-                    if (nums[mid] <= target) left = mid + 1;
-                    else right = mid;
-                }
-                return left;
-            }
-        }
-
         Helper helper = new Helper(nums, k, numOperations);
         int res = 1;
-        for (int i = 0; i < n; i++) {
-            res = Math.max(res, helper.check(nums[i]));
-            res = Math.max(res, helper.check(nums[i] - k));
-            res = Math.max(res, helper.check(nums[i] + k));
+        Set<Integer> numSet = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+        for (int num : numSet) {
+            res = Math.max(res, helper.check(num));
+            res = Math.max(res, helper.check(num - k));
+            res = Math.max(res, helper.check(num + k));
         }
 
         return res;
